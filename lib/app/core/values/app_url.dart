05aaf00c0b013cb9eedd2c3a01c4fc0350fc608b
Phạm Hoàng Sang <phsang49@gmail.com>
2024-01-06 10:11:51 +0700

    Implement fvm cho Mac

--- lib/app/core/values/app_url.dart
+++ lib/app/core/values/app_url.dart
@@ -1,4 +1,4 @@
-const String _domain = '192.168.43.175:3000';
+const String _domain = '192.168.82.93:3000';
 
 const String appWebsocketUrl = 'ws://$_domain';
 const String appUrl = 'http://$_domain/api/';
